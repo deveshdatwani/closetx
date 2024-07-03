9ba3b40e4598e8b32414f9a1a5acce313dd00f35
deveshdatwani <datwanidevesh@gmail.com>
2024-07-03 13:35:44 -0400

    registers users on sqlite3

---
+++ app/init_db.py
@@ -0,0 +1,14 @@
+from flask import g, current_app
+import click
+import sqlite3
+
+
+@click.command('init-db')
+def init_app_db():
+    db = sqlite3.connect(current_app.config['DATABASE'], 
+                         detect_types=sqlite3.PARSE_DECLTYPES)
+    with current_app.open_resource('/home/deveshdatwani/closetx/schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    db.close()
+    
+    return "SUCCESSFULLY INITIALIZED DATABASE"
--- db/db.py
+++ db/db.py
@@ -1,22 +1,30 @@
 import click
 import sqlite3
+from logging import Logger
 from flask import current_app, g
+log = Logger('db logger')
 
 
+@click.command('init-db')
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
+    
+    return "SUCCESS" 
 
 
 def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+    if 'db' not in g:    
+        if current_app.config['DATABASE']:
+            g.db = sqlite3.connect(
+                current_app.config['DATABASE'],
+                detect_types=sqlite3.PARSE_DECLTYPES
+            )
+            g.db.row_factory = sqlite3.Row
+        else:
+            log.warn("NO DATABASE CONFIG FOUND")
 
     return g.db
 
