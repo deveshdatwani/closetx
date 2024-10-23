fbae888f1a7f270b50ec2dc42267ed914c8298e0
deveshdatwani <datwanidevesh@gmail.com>
2024-10-23 19:58:29 -0400

    writes more error codes into error string class

--- app/lib/error_codes.py
+++ app/lib/error_codes.py
@@ -12,3 +12,4 @@ class ResponseString(BaseModel):
     user_deleted_succesfully: str = "USER DELETED SUCCESSFULLY"
     something_went_wrong: str = "SOMETHING WENT WRONG"
     registered_user_successfully: str = "200 SUCCESSFULLY REGISTERED USER"
+    hello: str = "HELLO"
