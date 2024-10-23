72b385503b53cd478d9d43a904c14969609aabda
deveshdatwani <datwanidevesh@gmail.com>
2024-10-23 17:18:17 -0400

    cleans error codes ;

--- app/lib/error_codes.py
+++ app/lib/error_codes.py
@@ -12,8 +12,3 @@ class ResponseString(BaseModel):
     user_deleted_succesfully: str = "USER DELETED SUCCESSFULLY"
     something_went_wrong: str = "SOMETHING WENT WRONG"
     registered_user_successfully: str = "200 SUCCESSFULLY REGISTERED USER"
-
-
-class hell():
-    def __init__(self):
-        self.value = None
