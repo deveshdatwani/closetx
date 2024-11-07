38c5d577cb1cb2feade0bd28ab17466a24ccd277
deveshdatwani <datwanidevesh@gmail.com>
2024-11-06 22:49:20 -0500

    stable apps, nothing much

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 
-class FeedForwad(nn.Module):
+class FeedForward(nn.Module):
     def __init__(self, in_dim, hidden_dim, out_dim):
         super().__init__()
         self.in_dim = in_dim
@@ -11,9 +11,9 @@ class FeedForwad(nn.Module):
         self.hidden_dim = hidden_dim
         self.layer1 = nn.Linear(in_dim, hidden_dim)
         self.layer2 = nn.Linear(hidden_dim, out_dim)
-        
 
     def forward(self, x):
-        x = nn.layer1(x)
+        x = self.layer1(x)
         x = nn.ReLU()(x)
-        x = nn.layer2(x)
+        x = self.layer2(x)
+        return x
