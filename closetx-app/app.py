82fbf7b2266b51094903770a3c24d045fa059724
deveshdatwani <datwanidevesh@gmail.com>
2024-12-15 14:32:16 -0500

    writes login required function as wrapper for all internal resources

--- closetx-app/app.py
+++ closetx-app/app.py
@@ -10,21 +10,17 @@ def create_app(config_file=None):
     app = Flask(__name__)
     app.loggerlogger = logging.getLogger('my_logger')
     app.logger.setLevel(logging.INFO)
+    app.config["SECRET"] = "closetx_secret"
     if config_file:
         try:
             app.config.from_file(config_file)
-            app.logger.info("Application configured succesfully")
+            app.logger.info("Application configured succesfully from config file")
         except Exception as e:
-            app.logger.error(f"Corrupt config file {e}")
+            app.logger.error(f"Corrupt config file")
     else:
-        app.logger.warning("NO config file found") 
-        app.apparel_folder = "./apparel"
-        app.salt = "salt"
-        app.pepper = "pepper"
+        app.logger.warning("No config file found") 
+        app.access_key = os.environ.get("ACCESS_KEY", default=None)
+        app.secret_key = os.environ.get("SECRET_KEY", default=None)
     app.register_blueprint(auth.auth)
     app.register_blueprint(apparel.apparel)
-    app.access_key = os.environ.get("ACCESS_KEY", default=None)
-    app.secret_key = os.environ.get("SECRET_KEY", default=None)
-    app.error_codes = ResponseString()
-    metrics = PrometheusMetrics(app)
     return app
