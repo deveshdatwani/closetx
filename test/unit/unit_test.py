bdc2a58ea1fdd78e3891aad34eacf71c9a125a10
deveshdatwani <datwanidevesh@gmail.com>
2024-12-18 20:32:19 -0500

    adds unit test for login and register

--- test/unit/unit_test.py
+++ test/unit/unit_test.py
@@ -6,4 +6,21 @@ import importlib
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"hacked" in response.data
+    assert b"Closetx" in response.data
+
+
+def test_user_registration(client):
+    print("Testing")
+    response = client.post('/register', data={"username":"deveshdatwani",
+                                           "emailid":"deveshd@bolt6.ai",
+                                           "password":"password"})
+    assert response.status_code == 200
+    assert "User registered successfully" in response.data
+
+
+def test_user_login(client):
+    print("Testing")
+    response = client.post('/login', data={"username":"deveshdatwani",
+                                           "password":"password"})
+    assert response.status_code == 200
+    assert "Login success" in response.data and "Use details" in response.data 
