849e77d6dca5b007bbff595c1c26e31bf78e1bd8
deveshdatwani <datwanidevesh@gmail.com>
2024-12-21 09:40:15 -0500

    commented out workflow

--- ml-app/serve_model.py
+++ ml-app/serve_model.py
@@ -10,12 +10,12 @@ from flask import Blueprint, g, request, redirect, current_app
 serve_model = Blueprint("serve_model", __name__)
 
 
-@serve_model.route('/', methods=['GET',])
+@serve_model.route('/model', methods=['GET',])
 def index():
-    return "Invalid URL"
+    return "Welcome to closetx engine"
 
 
-@serve_model.route('/apparel/match', methods=['GET', 'POST'])
+@serve_model.route('/model/apparel/match', methods=['GET', 'POST'])
 def match_apparel():
     top = request.files['top']
     top = Image.open(top)
