9f237264f5d2198cdfe9723c5f6d8938a6997015
deveshdatwani <datwanidevesh@gmail.com>
2024-12-21 20:40:28 -0500

    CHECKPOINT-- fully working system

---
+++ ml_app/models/apparel_encoder_models/infer.py
@@ -0,0 +1,8 @@
+import torch
+from model import EfficientNet
+
+if __name__ == '__main__':
+    _model = EfficientNet()
+    input_img = torch.rand(1, 3, 576, 576)
+    output = _model(input_img)
+    print(output.shape)
