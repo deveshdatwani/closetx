eef95bc771b3967e8fcf38267a5433bb77673cdf
deveshdatwani <datwanidevesh@gmail.com>
2024-12-27 14:58:00 -0500

    readjusting some stuff

--- ml_app/dataloader.py
+++ ml_app/dataloader.py
@@ -11,28 +11,34 @@ class CustomDataset(Dataset):
         self.raw_path = raw_path
         self.top_path = top_path
         self.bottom_path = bottom_path
-        self.transform = transforms.Compose([  transforms.Resize((576, 576)), 
+        self.transform = transforms.Compose([  transforms.Resize((26, 26)), 
                                                 transforms.ToTensor(), 
                                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                             ])
+        self.counter = 1
 
     def __len__(self):
         return len(os.listdir(self.top_path))
 
     def __getitem__(self, idx):
-        if random.randint(0,1) == 0:
+        if self.counter % 2:
+            idx = 2
             top_path = os.path.join(self.top_path, os.listdir(self.top_path)[idx])
             bottom_path = os.path.join(self.bottom_path, os.listdir(self.bottom_path)[idx])
             top_image = self.transform(Image.open(top_path))
             bottom_image = self.transform(Image.open(bottom_path))
             targets = 1
+            self.counter += 1
         else:
-            top_path = os.path.join(self.top_path, os.listdir(self.top_path)[idx])            
+            top_path = os.path.join(self.top_path, os.listdir(self.top_path)[idx])          
+            idx = 30
             idx_rand = random.choice(list(range(82)))
+            idx_rand = 31
             while idx_rand == idx:
                 idx_rand = random.choice(list(range(82)))
             bottom_path = os.path.join(self.bottom_path, os.listdir(self.bottom_path)[idx_rand])
             top_image = self.transform(Image.open(top_path))
             bottom_image = self.transform(Image.open(bottom_path))
             targets = 0
+            self.counter += 1
         return top_image, bottom_image, targets
--- ml_app/infer.py
+++ ml_app/infer.py
@@ -4,27 +4,29 @@ from time import time
 import cv2
 import torch 
 from collections import OrderedDict
-from models.apparel_encoder_models.model import EfficientNet
+from models.encoder.resnets import EfficientNet
 
 def inference(model, top, bottom):
     top = Image.open(top)
+    top = top.resize((26,26))
     top = np.array(top, np.float32)
-    top = cv2.resize(top,(576, 576))
     top = cv2.cvtColor(top, cv2.COLOR_BGR2RGB)
     bottom = Image.open(bottom)
+    bottom = bottom.resize((26,26))
     bottom = np.array(bottom, np.float32)
-    bottom = cv2.resize(bottom,(576, 576))
     bottom = cv2.cvtColor(bottom, cv2.COLOR_BGR2RGB)
     top = torch.permute(torch.from_numpy(top), (2, 0, 1))
     bottom = torch.permute(torch.from_numpy(bottom), (2, 0, 1))
+    top = torch.flatten(top, start_dim=0)
+    bottom = torch.flatten(bottom, start_dim=0)
     top = top.unsqueeze(0)
     bottom = bottom.unsqueeze(0)
     start_time = time()
     score = model(bottom, top)
     end_time = time()
     #current_app.logger.info(f"Inferenced {top.shape[0]} batches in {end_time - start_time} seconds")
+    print(f"Total time taken {end_time-start_time}")
     return score
-
 
 def get_model():
     model = EfficientNet()
@@ -33,7 +35,7 @@ def get_model():
     return model
 
 model = get_model()
-top = "/home/deveshdatwani/closetx/ml_app/models/dataset/positive/top/cn55717014.jpg" 
-bottom = "/home/deveshdatwani/closetx/ml_app/models/dataset/positive/bottom/cn56200152.jpg"
+top = "/home/deveshdatwani/closetx/ml_app/models/dataset/positive/top/cn56640342.jpg" 
+bottom = "/home/deveshdatwani/closetx/ml_app/models/dataset/positive/bottom/cn56940650.jpg"
 
 print(inference(model, top, bottom))
