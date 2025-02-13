6f0c50aa4a0c1fc6e6df71acc71542eb06c309cf
deveshdatwani <datwanidevesh@gmail.com>
2025-02-13 14:19:30 -0500

    model app now has API end points segments apparel and returns color match

--- ml_app/serve_model.py
+++ ml_app/serve_model.py
@@ -1,25 +1,45 @@
 import os
+import io
 from PIL import Image
 from io import BytesIO
-from ml_app.utils import *
-from flask import Blueprint, request, current_app
-from ml_app.utils import get_median_pixel
+from ml_app.lib.utils import *
+from flask import Blueprint, request, current_app, send_file
+from .models.encoder.color_encoder import get_palette_color
 from .models.encoder.color_encoder import palette_rbg_list as palette
+
 
 serve_model = Blueprint("serve_model", __name__, url_prefix="/model")
 
 
 @serve_model.route("/")
 def index():
-    return "Closetx says, Hi"
+    return "Closetx model says, Hi"
+
+
+@serve_model.route("/segment", methods=['POST'])
+def segment():
+    image = request.files['image']
+    segmented_image = seg_apparel(image, current_app.segmentation_model)
+    img_io = return_segmented_image(segmented_image)
+    return send_file(img_io, mimetype="image/png")
+
+
+@serve_model.route("/color", methods=['POST',])
+def get_color():
+    r, g, b = int(request.form['r']), int(request.form['g']), int(request.form['b'])
+    palette_color = get_palette_color((r,g,b))
+    return palette_color
+
+
+# -------------- #
 
 
 @serve_model.route("/match", methods=['POST',])
-def match():
+def match_apparels():
     top_image = request.files["top"]
     bottom_image = request.files["bottom"]
     top_color, bottom_color = get_outfit_colors(top_image, bottom_image, current_app.segmentation_model)
-    match = get_match(top_color, bottom_color)
+    match = get_palette_color(top_color, bottom_color)
     return str(match)
 
 
