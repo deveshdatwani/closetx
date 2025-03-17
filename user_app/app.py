ca59d75da00b6d8aa04d37f462268a2e165d5d97
deveshdatwani <datwanidevesh@gmail.com>
2025-03-17 17:46:59 -0400

    corrects prefix in nginx proxy pass and removes user app cache

--- user_app/app.py
+++ user_app/app.py
@@ -2,21 +2,14 @@ import os
 import logging  
 from . import auth, apparel
 from flask import Flask
-from .lib.config import config_cache_folder
 
-
-# experimental version for alpha MVP
 
 def create_app(config_file=None): 
     app = Flask(__name__)
     app.loggerlogger = logging.getLogger('my_logger')
     app.logger.setLevel(logging.INFO)
-    app.config["secret"] = "closetx_secret"
     app.register_blueprint(auth.auth)
     app.register_blueprint(apparel.apparel)
-    app.config["CACHE_DIR"] = "cache/"
-    config_cache_folder(app.config["CACHE_DIR"])
-    app.logger.info("Cleared and created cache directory")
     if config_file:
         try:
             app.config.from_file(config_file)
@@ -27,4 +20,5 @@ def create_app(config_file=None):
         app.logger.warning("No config file found") 
         app.config["access_key"] = os.environ.get("AWS_ACCESS_KEY", default=None)
         app.config["secret_key"] = os.environ.get("AWS_SECRET_KEY", default=None)
+        app.config["secret"] = "closetx_secret"
     return app
