503fcb12900f1cb007613d92d28dae570e72eaf5
deveshdatwani <datwanidevesh@gmail.com>
2025-04-08 13:49:47 -0400

    wip

--- ml_app/model_app.py
+++ ml_app/model_app.py
@@ -12,7 +12,7 @@ def index():
     return "Closetx model says, Hi"
 
 
-@serve_model.route("/segment", methods=['POST',])
+@serve_model.route("/segment", methods=['POST'])
 def segment():
     image = request.files['image']
     segmented_image = seg_apparel(image, current_app.segmentation_model)
@@ -20,7 +20,7 @@ def segment():
     return send_file(img_io, mimetype="image/png")
 
 
-@serve_model.route("/color", methods=['POST',])
+@serve_model.route("/get-color", methods=['POST',])
 def get_color():
     r, g, b = int(request.form['r']), int(request.form['g']), int(request.form['b'])
     palette_color = get_palette_color((r,g,b))
@@ -35,12 +35,19 @@ def match_color():
     return str(match_result)
 
 
-# @serve_model.route("/get-color-from-apparel", methods=['POST',])
-# def get_color_from_apparel():
-#     image = request.files['image']
-#     segmented_image = seg_apparel(image, current_app.segmentation_model)
-#     median_r, median_g, median_b = get_median_pixel(segmented_image)
-#     color = get_palette_color((median_r, median_g, median_b))
-#     color_rgb = palette_numbers[palette_names.index(color)]
-#     json_response = jsonify({"color":color, "r":color_rgb[0], "g":color_rgb[1], "b":color_rgb[2]})
-#     return json_response
+@serve_model.route("/get-color-from-apparel", methods=['POST',])
+def get_color_from_apparel():
+    image = request.files['image']
+    segmented_image = seg_apparel(image, current_app.segmentation_model)
+    plt.imshow(segmented_image)
+    plt.show()
+    median_r, median_g, median_b = get_median_pixel(segmented_image)
+    color = get_palette_color((median_r, median_g, median_b))
+    return jsonify({"color":color, "r":median_r, "g":median_g, "b":median_b})
+
+
+@serve_model.route("/classify", methods=['POST',])
+def classify_apparel():
+    image = request.files['image']
+    label = classify_from_image(image, current_app.segmentation_model)
+    return label
--- ml_app/models/encoder/palette.py
+++ ml_app/models/encoder/palette.py
@@ -1,4 +1,4 @@
-palette_rgb = {
+palette_rgb = [
     (243, 199, 189),
     (210, 66, 71),
     (241, 89, 42),
@@ -8,9 +8,9 @@ palette_rgb = {
     (200, 223, 236),
     (26, 72, 113),
     (115, 71, 90),
-    (99, 75, 71),
+    (152, 105, 63),
     (174, 182, 189),
-}
+]
 
 palette_match = {
     "pink": ["lightblue", "darkblue", "grey", "white", "black"],
@@ -36,6 +36,6 @@ palette_names = {
     (200, 223, 236): "lightblue",
     (26, 72, 113): "darkblue",
     (115, 71, 90): "purple",
-    (99, 75, 71): "brown",
+    (152, 105, 63): "brown",
     (174, 182, 189): "grey"
 }
